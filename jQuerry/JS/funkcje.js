b6fbed4b6a54dbbafa4d0af275ca9b36d3cc4818
Mikołaj Szuper <szumikolaj@icloud.com>
2024-05-22 21:09:33 +0200

    jQuerry

--- jQuerry/JS/funkcje.js
+++ jQuerry/JS/funkcje.js
@@ -2,6 +2,24 @@ $(document).ready(function () {
     $('#tresc').css('background','grey');
     $('input').css('font-weight','bold');
     $('.zielony').css('background','lime');
+    $("#oblicz").click(function(){
+        var K=parseInt( $('#K').val() );
+        var pr=parseInt( $('#pr').val() );
+        var n=parseInt( $('#n').val() );
+        var pr_mc= 1 + (pr/100/12);
+        if (!isNaN(K)&&isFinite(K)&&!isNaN(n)&&isFinite(n)&&!isNaN(pr)&&isFinite(pr)) {
+            var oblizanie = (K * Math.pow(pr_mc,n) * (pr_mc - 1) / (Math.pow(pr_mc,n) - 1)).toFixed(2);
+            $('#rata_cala').val(oblizanie*n);
+            $('#rata').val(oblizanie);
+            if (isNaN(oblizanie*n)) {
+                $('#rata_cala').val("");
+                $('#rata').val("Błędne dane");
+            }
+            //w.value=((K*pr_mc)/(1-(1/(Math.pow((1+pr_mc),n)))));
+        } else {
+            $('#rata').val("Błędne dane");
+        }
+         });        
 });
 
 function oblicz()
